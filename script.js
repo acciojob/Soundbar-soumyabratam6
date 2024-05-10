e04161fc3d5c5b665b917eb6eea373fc42e8c235
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-05-10 07:27:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function playSound(soundFileName) {
+  var audio = new Audio('sounds/' + soundFileName);
+  audio.play();
+}
+
+function stopAllSounds() {
+  var sounds = document.getElementsByTagName('audio');
+  for (var i = 0; i < sounds.length; i++) {
+    sounds[i].pause();
+    sounds[i].currentTime = 0;
+  }
+}
